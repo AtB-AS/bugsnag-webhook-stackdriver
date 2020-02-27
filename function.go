ae013b8cd46ab2f605227bcb71f7ca3c6e7be34c
Torbjørn Fjørtoft <torfjor@fjortoft.org>
2020-02-27 19:57:41 +0100

    initial webhook skeleton

--- function.go
+++ function.go
@@ -1,11 +1,45 @@
 package function
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/atb-as/bugsnag-webhook-stackdriver/bugsnag"
 )
 
-// HelloHTTP is an HTTP Cloud function
-func HelloHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, world!")
+var (
+	projectID string
+)
+
+func init() {
+	env, ok := os.LookupEnv("GCP_PROJECT")
+	projectID = env
+
+	if !ok {
+		log.Fatal("GCP_PROJECT not found in environment")
+	}
+}
+
+// BugsnagWebhook is a HTTP webhook for receiving errors from bugsnag
+func BugsnagWebhook(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+		return
+	}
+
+	trigger := &bugsnag.Trigger{}
+	err = json.Unmarshal(body, trigger)
+	if err != nil {
+		http.Error(w, "Failed to parse request body", http.StatusBadRequest)
+		return
+	}
+
+	// Do something with trigger here
+
+	fmt.Fprintf(w, "OK")
 }
